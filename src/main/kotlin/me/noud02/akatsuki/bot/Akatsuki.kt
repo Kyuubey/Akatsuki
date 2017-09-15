4460b6cf19d5af755ebeca3b2a0fec425b3b26ce
noud02 <noud.kerver@hotmail.nl>
2017-09-15 22:15:18 +0200

    awoo

---
+++ src/main/kotlin/me/noud02/akatsuki/bot/Akatsuki.kt
@@ -0,0 +1,45 @@
+package me.noud02.akatsuki.bot
+
+import net.dv8tion.jda.core.AccountType
+import net.dv8tion.jda.core.JDA
+import net.dv8tion.jda.core.JDABuilder
+import net.dv8tion.jda.core.entities.Game
+import net.dv8tion.jda.core.events.ReadyEvent
+import net.dv8tion.jda.core.events.guild.GuildJoinEvent
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent
+import net.dv8tion.jda.core.hooks.ListenerAdapter
+import org.jetbrains.exposed.sql.Database
+
+class Akatsuki(token: String, db_name: String, db_user: String, db_password: String) : ListenerAdapter() {
+
+    val jda: JDA = JDABuilder(AccountType.BOT)
+            .setToken(token)
+            .addEventListener(this)
+            .buildBlocking()
+
+    private val cmdHandler: CommandHandler = CommandHandler(this)
+    private val eventHandler: EventHandler = EventHandler(this)
+
+    var botPrefix: String = "!"
+    val db = Database.connect(db_name, driver = "org.postgresql.Driver", password = db_password, user = db_user)
+
+    fun setPrefix(prefix: String) {
+        botPrefix = prefix
+    }
+
+    fun setGame(text: String, idle: Boolean = false) {
+        jda.presence.setPresence(Game.of(text), idle)
+    }
+
+    override fun onMessageReceived(event: MessageReceivedEvent) {
+        cmdHandler.handle(event)
+    }
+
+    override fun onReady(event: ReadyEvent) {
+        println("Ready!")
+    }
+
+    override fun onGuildJoin(event: GuildJoinEvent) {
+        eventHandler.guildJoin(event)
+    }
+}
