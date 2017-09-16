b6e10da85c789ef2a2a2cfc16152aae4670eb784
noud02 <noud.kerver@hotmail.nl>
2017-09-16 12:40:50 +0200

    use annotations for args

--- src/main/kotlin/me/noud02/akatsuki/bot/entities/Argument.kt
+++ src/main/kotlin/me/noud02/akatsuki/bot/entities/Argument.kt
@@ -1,3 +1,3 @@
 package me.noud02.akatsuki.bot.entities
 
-class Argument(val name: String, val type: String, val optional: Boolean = false)
+annotation class Argument(val name: String, val type: String, val optional: Boolean = false)
--- src/main/kotlin/me/noud02/akatsuki/bot/entities/Command.kt
+++ src/main/kotlin/me/noud02/akatsuki/bot/entities/Command.kt
@@ -2,7 +2,6 @@ package me.noud02.akatsuki.bot.entities
 
 open class Command {
     val subcommands: MutableMap<String, Command> = mutableMapOf()
-    val args: MutableList<Argument> = mutableListOf()
     open val name = "awoo"
     open val desc = "awoo~"
     open val ownerOnly = false
@@ -13,9 +12,5 @@ open class Command {
 
     fun addSubcommand(cmd: Command) {
         subcommands[cmd.name] = cmd
-    }
-
-    fun setArgument(name: String, type: String, optional: Boolean = false) {
-        args.add(Argument(name, type, optional))
     }
 }
