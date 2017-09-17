2c68c51065965e8add0238bb8c575f52eb9906bc
yuwui <me@yui.pet>
2017-09-17 10:13:32 +0200

    flags owo

--- src/main/kotlin/me/noud02/akatsuki/commands/Ping.kt
+++ src/main/kotlin/me/noud02/akatsuki/commands/Ping.kt
@@ -1,15 +1,31 @@
 package me.noud02.akatsuki.commands
 
+import khttp.get
 import me.noud02.akatsuki.bot.entities.Argument
 import me.noud02.akatsuki.bot.entities.Command
 import me.noud02.akatsuki.bot.entities.Context
+import me.noud02.akatsuki.bot.entities.Flag
 import net.dv8tion.jda.core.entities.Member
+import java.util.*
 
+@Flag("meme", 'm', "this is a meme")
 class Pong : Command() {
     override val name = "pong"
     override val desc = "Ping!"
 
-    override fun run(ctx: Context) = ctx.send("Ping!")
+    override fun run(ctx: Context) {
+        if (ctx.flags.argMap.containsKey("meme")) {
+            val res = get("https://www.reddit.com/r/dankmemes.json")
+                    .jsonObject
+                    .getJSONObject("data")
+                    .getJSONArray("children")
+                    .getJSONObject(Math.floor(Math.random() * 10).toInt())
+                    .getJSONObject("data")
+                    .getString("url")
+            ctx.send(res)
+        } else
+            ctx.send("Ping!")
+    }
 }
 
 @Argument("meme", "user", true)
