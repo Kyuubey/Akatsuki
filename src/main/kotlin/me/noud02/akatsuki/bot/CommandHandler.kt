7cee31e88f30b1ea5f152cf31878893c2dd94360
yuwui <me@yui.pet>
2017-09-17 10:14:05 +0200

    add help

--- src/main/kotlin/me/noud02/akatsuki/bot/CommandHandler.kt
+++ src/main/kotlin/me/noud02/akatsuki/bot/CommandHandler.kt
@@ -1,9 +1,7 @@
 package me.noud02.akatsuki.bot
 
-import me.noud02.akatsuki.bot.entities.Argument
-import me.noud02.akatsuki.bot.entities.Command
-import me.noud02.akatsuki.bot.entities.Context
-import me.noud02.akatsuki.bot.entities.Perm
+import me.aurieh.ares.utils.ArgParser
+import me.noud02.akatsuki.bot.entities.*
 import net.dv8tion.jda.core.Permission
 import net.dv8tion.jda.core.entities.Channel
 import net.dv8tion.jda.core.entities.Member
@@ -13,7 +11,7 @@ import org.reflections.util.ClasspathHelper
 import org.reflections.util.ConfigurationBuilder
 
 class CommandHandler(private val client: Akatsuki) {
-    private val commands = mutableMapOf<String, Command>()
+    val commands = mutableMapOf<String, Command>()
 
     init {
         loadAll()
@@ -54,42 +52,63 @@ class CommandHandler(private val client: Akatsuki) {
             if ((commands[cmd] as Command).ownerOnly && !client.owners.contains(event.author.id))
                 return
 
+            var command = commands[cmd] as Command
+
             if (args.isNotEmpty() && commands[cmd]?.subcommands?.get(args[0]) is Command) {
                 val subcmd = args[0]
+                command = commands[cmd]?.subcommands?.get(subcmd) as Command
                 args = args.drop(1)
 
+                val flags = ArgParser.untypedParseSplit(ArgParser.tokenize(args.joinToString(" ")))
+
+                args = flags.unmatched
+
+                if (!command.noHelp && (flags.argMap.contains("h") || flags.argMap.contains("help"))) {
+                    event.channel.sendMessage(help(command)).complete()
+                    return
+                }
+
                 try {
-                    checkPermissions(event, commands[cmd]?.subcommands?.get(subcmd))
-                    newArgs = checkArguments(event, commands[cmd]?.subcommands?.get(subcmd), args)
+                    checkPermissions(event, command)
+                    newArgs = checkArguments(event, command, args)
                 } catch (err: Exception) {
                     event.channel.sendMessage(err.message).complete()
                     return
                 }
 
+                ctx = Context(event, client, command, newArgs, args, flags)
+
+                if (!command.noHelp && (flags.argMap.contains("h") || flags.argMap.contains("help")))
+                    return ctx.send(ctx.help())
 
 
-                ctx = Context(event, client, newArgs, args)
-                commands[cmd]?.subcommands?.get(subcmd)?.run(ctx)
+                command.run(ctx)
             } else {
+                val flags = ArgParser.untypedParseSplit(ArgParser.tokenize(args.joinToString(" ")))
+
+                args = flags.unmatched
+
+                if (!command.noHelp && (flags.argMap.contains("h") || flags.argMap.contains("help"))) {
+                    event.channel.sendMessage(help(command)).complete()
+                    return
+                }
+
                 try {
-                    checkPermissions(event, commands[cmd])
-                    newArgs = checkArguments(event, commands[cmd], args)
+                    checkPermissions(event, commands[cmd] as Command)
+                    newArgs = checkArguments(event, commands[cmd] as Command, args)
                 } catch (err: Exception) {
                     event.channel.sendMessage(err.message).complete()
                     return
                 }
 
-                ctx = Context(event, client, newArgs, args)
+                ctx = Context(event, client, commands[cmd] as Command, newArgs, args, flags)
 
                 commands[cmd]?.run(ctx)
             }
         }
     }
 
-    private fun checkPermissions(event: MessageReceivedEvent, cmd: Command?) {
-        if (cmd == null)
-            throw Exception("Command is null")
-
+    private fun checkPermissions(event: MessageReceivedEvent, cmd: Command) {
         val perms: List<Perm> = cmd::class.annotations.filterIsInstance(Perm::class.java)
         val newPerms = mutableMapOf<Permission, Boolean>()
 
@@ -100,10 +119,7 @@ class CommandHandler(private val client: Akatsuki) {
         }
     }
 
-    private fun checkArguments(event: MessageReceivedEvent, cmd: Command?, args: List<String>): MutableMap<String, Any> {
-        if (cmd == null)
-            throw Exception("Command is null")
-
+    private fun checkArguments(event: MessageReceivedEvent, cmd: Command, args: List<String>): MutableMap<String, Any> {
         val newArgs = mutableMapOf<String, Any>()
 
         val cmdArgs: List<Argument> = cmd::class.annotations.filterIsInstance(Argument::class.java)
@@ -145,5 +161,40 @@ class CommandHandler(private val client: Akatsuki) {
         }
 
         return newArgs
+    }
+
+    fun help(cmdd: String): String {
+        if (!commands.contains(cmdd))
+            throw Exception("No such command: $cmdd")
+
+        val cmd = commands[cmdd] as Command
+        val args = cmd::class.annotations.filterIsInstance(Argument::class.java)
+        val flags = cmd::class.annotations.filterIsInstance(Flag::class.java)
+
+        val sub = cmd.subcommands.map { entry: Map.Entry<String, Command> -> "\t${entry.value.name}" + " ".repeat(20 - entry.value.name.length) + "${entry.value.desc}\n" }
+        val flag = flags.map { flag: Flag -> "\t${flag.abbr}, ${flag.flag}" + " ".repeat(20 - "${flag.abbr}, ${flag.flag}".length) + flag.desc + "\n"}
+        val usage = args.map { argument: Argument -> if (argument.optional) "[${argument.name}: ${argument.type}]" else "<${argument.name}: ${argument.type}>" }
+        val formattedSubs = if (sub.isNotEmpty()) "\nSubcommands:\n${sub.joinToString("\n")}" else ""
+        val formattedFlags = if (flag.isNotEmpty()) "\nFlags:\n${flag.joinToString("\n")}" else ""
+
+        val formatted = "```\n${cmd.name} ${usage.joinToString(" ")}\n\n${cmd.desc}\n$formattedSubs$formattedFlags```"
+
+        return formatted
+    }
+
+    fun help(cmdd: Command): String {
+        val cmd = cmdd
+        val args = cmd::class.annotations.filterIsInstance(Argument::class.java)
+        val flags = cmd::class.annotations.filterIsInstance(Flag::class.java)
+
+        val sub = cmd.subcommands.map { entry: Map.Entry<String, Command> -> "\t${entry.value.name}" + " ".repeat(20 - entry.value.name.length) + "${entry.value.desc}\n" }
+        val flag = flags.map { flag: Flag -> "\t-${flag.abbr}, --${flag.flag}" + " ".repeat(20 - "-${flag.abbr}, --${flag.flag}".length) + flag.desc + "\n"}
+        val usage = args.map { argument: Argument -> if (argument.optional) "[${argument.name}: ${argument.type}]" else "<${argument.name}: ${argument.type}>" }
+        val formattedSubs = if (sub.isNotEmpty()) "\nSubcommands:\n${sub.joinToString("\n")}" else ""
+        val formattedFlags = if (flag.isNotEmpty()) "\nFlags:\n${flag.joinToString("\n")}" else ""
+
+        val formatted = "```\n${cmd.name} ${usage.joinToString(" ")}\n\n${cmd.desc}\n$formattedSubs$formattedFlags```"
+
+        return formatted
     }
 }
