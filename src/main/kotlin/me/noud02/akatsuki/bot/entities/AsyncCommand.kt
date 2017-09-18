c8e2d5440e976135f24214e4535dfd35a779e1d8
noud02 <noud.kerver@hotmail.nl>
2017-09-18 15:39:24 +0200

    add async command thing

--- src/main/kotlin/me/noud02/akatsuki/bot/entities/AsyncCommand.kt
+++ src/main/kotlin/me/noud02/akatsuki/bot/entities/AsyncCommand.kt
@@ -1,4 +1,22 @@
 package me.noud02.akatsuki.bot.entities
 
-class AsyncCommand {
+import kotlinx.coroutines.experimental.async
+
+open class AsyncCommand {
+    val subcommands = mutableMapOf<String, Command>()
+    open val name = "awoo"
+    open val desc = "awoo~"
+    open val ownerOnly = false
+    open val noHelp = false
+    open val guildOnly = false
+
+    open suspend fun asyncRun(ctx: Context) = ctx.send("Empty command")
+
+    fun run(ctx: Context) {
+        async()
+    }
+
+    fun addSubcommand(cmd: Command) {
+        subcommands[cmd.name] = cmd
+    }
 }
