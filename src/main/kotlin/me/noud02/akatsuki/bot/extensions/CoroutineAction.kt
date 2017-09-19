622be9e0a151fb09abc237ae6915f832ba27d05f
aurieh <me@aurieh.me>
2017-09-19 18:42:50 +0300

    Fix CoroutineAction.await()

--- src/main/kotlin/me/noud02/akatsuki/bot/extensions/CoroutineAction.kt
+++ src/main/kotlin/me/noud02/akatsuki/bot/extensions/CoroutineAction.kt
@@ -2,13 +2,5 @@ package me.noud02.akatsuki.bot.extensions
 
 import kotlinx.coroutines.experimental.future.await
 import net.dv8tion.jda.core.requests.RestAction
-import net.dv8tion.jda.core.requests.restaction.CompletedFuture
-import java.util.concurrent.CompletableFuture
 
-suspend fun<V> RestAction<V>.await(): V {
-    // As `RestAction#submit()` already returns a subclass of CompletableFuture
-    // we can just cast to it
-    val fut = submit()
-    if (fut is CompletedFuture<V>) return fut.get()
-    return (fut as CompletableFuture<V>).await()
-}
+suspend fun<V> RestAction<V>.await() = submit().await()
