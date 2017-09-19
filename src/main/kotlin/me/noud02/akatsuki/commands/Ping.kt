a340184bd7b118b2f22395abca766c995a897c2e
yuwui <me@yui.pet>
2017-09-19 18:32:24 +0200

    make ping async

--- src/main/kotlin/me/noud02/akatsuki/commands/Ping.kt
+++ src/main/kotlin/me/noud02/akatsuki/commands/Ping.kt
@@ -2,6 +2,7 @@ package me.noud02.akatsuki.commands
 
 import khttp.get
 import me.noud02.akatsuki.bot.entities.*
+import me.noud02.akatsuki.bot.extensions.await
 import net.dv8tion.jda.core.entities.Member
 import net.dv8tion.jda.core.entities.Message
 import java.time.temporal.ChronoUnit
@@ -28,7 +29,7 @@ class Pong : Command() {
 
 @Load
 @Argument("meme", "user", true)
-class Ping : Command() {
+class Ping : AsyncCommand() {
     override val name = "ping"
     override val desc = "Pings a user if specified"
 
@@ -36,14 +37,13 @@ class Ping : Command() {
         addSubcommand(Pong())
     }
 
-    override fun run(ctx: Context) {
+    override suspend fun asyncRun(ctx: Context) {
         if (ctx.args["meme"] != null && ctx.args["meme"] is Member) {
             val user = ctx.args["meme"] as Member
             ctx.send(user.asMention)
         } else {
-            ctx.event.channel.sendMessage("Pong!").queue({ message: Message ->
-                message.editMessage("Pong! `${ctx.msg.creationTime.until(message.creationTime, ChronoUnit.MILLIS)}ms`").queue()
-            })
+            val msg = ctx.event.channel.sendMessage("Pong!").await()
+            msg.editMessage("Pong! `${ctx.msg.creationTime.until(msg.creationTime, ChronoUnit.MILLIS)}ms`").queue()
         }
     }
 }
