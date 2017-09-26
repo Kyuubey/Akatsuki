3e2f9f05d32d804170821fe023b462037f29b331
yuwui <me@yui.pet>
2017-09-26 16:45:49 +0200

    locales awoo

--- src/main/kotlin/me/noud02/akatsuki/bot/entities/Context.kt
+++ src/main/kotlin/me/noud02/akatsuki/bot/entities/Context.kt
@@ -4,17 +4,15 @@ import me.aurieh.ares.utils.ArgParser
 import me.noud02.akatsuki.bot.Akatsuki
 import net.dv8tion.jda.core.entities.*
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
-import org.apache.commons.validator.routines.UrlValidator
+import java.util.*
 
-class Context(val event: MessageReceivedEvent, val client: Akatsuki, private val cmd: Command, val args: MutableMap<String, Any>, val rawArgs: List<String>, val flags: ArgParser.ParsedResult, val perms: MutableMap<String, Boolean>) {
+class Context(val event: MessageReceivedEvent, val client: Akatsuki, private val cmd: Command, val args: MutableMap<String, Any>, val rawArgs: List<String>, val flags: ArgParser.ParsedResult, val perms: MutableMap<String, Boolean>, val lang: ResourceBundle) {
     val guild: Guild? = event.guild
     val author: User = event.author
     val channel: MessageChannel = event.channel
     val msg: Message = event.message
     val member: Member? = event.member
     val selfMember: Member? = event.guild.selfMember
-
-    val urlValidator = UrlValidator()
 
     fun send(arg: String) = event.channel.sendMessage(arg).queue()
     fun send(arg: MessageEmbed) = event.channel.sendMessage(arg).queue()
