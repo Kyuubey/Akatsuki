365e53a9fa34ab1de08e479a38475c3bb4591d7f
noud02 <noud.kerver@hotmail.nl>
2017-10-02 14:59:32 +0200

    forgot to @Load this

--- src/main/kotlin/me/noud02/akatsuki/commands/Pause.kt
+++ src/main/kotlin/me/noud02/akatsuki/commands/Pause.kt
@@ -2,10 +2,12 @@ package me.noud02.akatsuki.commands
 
 import me.noud02.akatsuki.bot.entities.Command
 import me.noud02.akatsuki.bot.entities.Context
+import me.noud02.akatsuki.bot.entities.Load
 import me.noud02.akatsuki.bot.entities.Perm
 import me.noud02.akatsuki.bot.music.MusicManager
 import net.dv8tion.jda.core.Permission
 
+@Load
 @Perm(Permission.MANAGE_SERVER)
 class Pause : Command() {
     override val name = "pause"
