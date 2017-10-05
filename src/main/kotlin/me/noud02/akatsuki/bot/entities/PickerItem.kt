c9bb99502ac27f6d137bddf7af829b152875dacd
noud02 <noud.kerver@hotmail.nl>
2017-10-05 18:29:29 +0200

    add item picker

---
+++ src/main/kotlin/me/noud02/akatsuki/bot/entities/PickerItem.kt
@@ -0,0 +1,5 @@
+package me.noud02.akatsuki.bot.entities
+
+import java.awt.Color
+
+class PickerItem (var id: String, var title: String = "", var description: String = "", var author: String = "", var image: String = "", var thumbnail: String = "", var footer: String = "", var color: Color? = null)
