4a2bb37d55a9ce1aeb24758b4e96b1de7106bec9
yuwui <me@yui.pet>
2017-11-11 16:25:16 +0100

    clean up birb command

--- src/main/kotlin/me/noud02/akatsuki/commands/Birb.kt
+++ src/main/kotlin/me/noud02/akatsuki/commands/Birb.kt
@@ -25,7 +25,6 @@
 
 package me.noud02.akatsuki.commands
 
-import khttp.get
 import me.noud02.akatsuki.bot.entities.Command
 import me.noud02.akatsuki.bot.entities.Context
 import me.noud02.akatsuki.bot.entities.Load
@@ -35,8 +34,5 @@ class Birb : Command() {
     override val name = "birb"
     override val desc = "Get a random birb"
 
-    override fun run(ctx: Context) {
-        val birb = get("https://random.birb.pw/tweet").text
-        ctx.send("https://random.birb.pw/img/$birb")
-    }
+    override fun run(ctx: Context) = ctx.send("https://random.birb.pw/img/${khttp.get("https://random.birb.pw/tweet").text}")
 }
