1de3acc4f98200e450e5ac7876fa3c678f10d45c
noud02 <noud.kerver@hotmail.nl>
2017-11-24 15:19:00 +0100

    add pout command

---
+++ src/main/kotlin/me/noud02/akatsuki/commands/Pout.kt
@@ -0,0 +1,42 @@
+/*
+ *  Copyright (c) 2017 Noud Kerver
+ *
+ *  Permission is hereby granted, free of charge, to any person
+ *  obtaining a copy of this software and associated documentation
+ *  files (the "Software"), to deal in the Software without
+ *  restriction, including without limitation the rights to use,
+ *  copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following
+ *  conditions:
+ *
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *  OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package me.noud02.akatsuki.commands
+
+import me.noud02.akatsuki.annotations.Load
+import me.noud02.akatsuki.entities.Command
+import me.noud02.akatsuki.entities.Context
+import me.noud02.akatsuki.utils.Wolk
+import me.noud02.akatsuki.utils.WolkType
+import net.dv8tion.jda.core.EmbedBuilder
+import java.awt.Color
+
+@Load
+class Pout : Command() {
+    override fun run(ctx: Context) = ctx.send(EmbedBuilder().apply {
+        setImage(Wolk.getByType(WolkType.POUT).url)
+        setColor(Color.CYAN)
+    }.build())
+}
