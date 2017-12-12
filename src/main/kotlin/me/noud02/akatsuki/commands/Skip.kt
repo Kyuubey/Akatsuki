4591771608633b67bd208a3647c86eb8d4c2597d
yuwui <me@yui.pet>
2017-12-12 18:22:40 +0000

    make some things look nicer

--- src/main/kotlin/me/noud02/akatsuki/commands/Skip.kt
+++ src/main/kotlin/me/noud02/akatsuki/commands/Skip.kt
@@ -44,17 +44,32 @@ class Skip : Command() {
 
     override fun run(ctx: Context) {
         if (!ctx.member!!.voiceState.inVoiceChannel())
-            return ctx.send(I18n.parse(ctx.lang.getString("join_voice_channel_fail"), mapOf("username" to ctx.author.name))) // TODO rename translation
+            return ctx.send(
+                    I18n.parse(
+                            ctx.lang.getString("join_voice_channel_fail"),
+                            mapOf(
+                                    "username" to ctx.author.name // TODO rename translation
+                            )
+                    )
+            )
         val manager = MusicManager.musicManagers[ctx.guild!!.id] ?: return ctx.send("Not connected!")
 
         if (ctx.perms["MANAGE_SERVER"] == true) {
             manager.scheduler.next()
-            ctx.send(I18n.parse(ctx.lang.getString("force_skip"), mapOf("username" to ctx.author.name)))
+            ctx.send(
+                    I18n.parse(
+                            ctx.lang.getString("force_skip"),
+                            mapOf(
+                                    "username" to ctx.author.name
+                            )
+                    )
+            )
         } else {
-            val members = manager.voiceChannel.members.filter { member: Member -> !member.user.isBot }
+            val members = manager.voiceChannel.members.filter { !it.user.isBot }
 
             if (members.size - 1 <= manager.voteSkip.size) {
                 manager.scheduler.next()
+
                 return ctx.send(ctx.lang.getString("voteskip_success"))
             }
 
@@ -63,11 +78,20 @@ class Skip : Command() {
 
             if (members.size - 1 <= manager.voteSkip.size + 1) {
                 manager.scheduler.next()
+
                 return ctx.send(ctx.lang.getString("voteskip_success"))
             }
 
             manager.voteSkip.add(ctx.author.id)
-            ctx.send(I18n.parse(ctx.lang.getString("voteskip_add_success"), mapOf("votes" to manager.voteSkip.size, "total_votes" to members.size - 1)))
+            ctx.send(
+                    I18n.parse(
+                            ctx.lang.getString("voteskip_add_success"),
+                            mapOf(
+                                    "votes" to manager.voteSkip.size,
+                                    "total_votes" to members.size - 1
+                            )
+                    )
+            )
         }
     }
 }
