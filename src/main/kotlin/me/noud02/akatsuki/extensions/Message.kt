24d27ea2726806e519d649f79190228d6d4756e7
yuwui <me@yui.pet>
2017-12-15 16:35:38 +0000

    add more vals to Logs

--- src/main/kotlin/me/noud02/akatsuki/extensions/Message.kt
+++ src/main/kotlin/me/noud02/akatsuki/extensions/Message.kt
@@ -32,6 +32,7 @@ import org.jetbrains.exposed.sql.insert
 import com.google.gson.Gson
 import net.dv8tion.jda.core.entities.Message
 import org.json.JSONObject
+import java.time.temporal.ChronoField
 
 
 fun Message.log(ev: String = "CREATE") {
@@ -44,12 +45,16 @@ fun Message.log(ev: String = "CREATE") {
             it[content] = contentRaw
             it[attachments] = this@log.attachments.map { it.url }.toTypedArray()
             it[embeds] = this@log.embeds.map { JSONObject(gson.toJson(it)) }.toTypedArray()
+            it[timestamp] = this@log.creationTime.toInstant().toEpochMilli() //.getLong(ChronoField.MILLI_OF_SECOND)
             it[authorId] = author.idLong
             it[authorName] = author.name
             it[authorDiscrim] = author.discriminator
             it[authorAvatar] = author.avatarUrl
             it[authorNick] = member?.nickname ?: ""
             it[guildId] = guild.idLong
+            it[guildName] = guild.name
+            it[channelId] = channel.idLong
+            it[channelName] = channel.name
         }
     }.execute().get()
 }
