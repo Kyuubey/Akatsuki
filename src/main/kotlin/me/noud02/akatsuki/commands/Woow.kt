13ef8d3ff51256f00870ccd04cdb97c1ff0534bf
yuwui <me@yui.pet>
2018-01-02 21:06:47 +0100

    add woow command

---
+++ src/main/kotlin/me/noud02/akatsuki/commands/Woow.kt
@@ -0,0 +1,58 @@
+/*
+ *  Copyright (c) 2017 Noud Kerver
+ *
+ *  Permission is hereby granted, free of charge, to any person
+ *  obtaining a copy of this software and associated documentation
+ *  files (the "Software"), to deal in the Software without
+ *  restriction, including without limitation the rights to use,
+ *  copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following
+ *  conditions:
+ *
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *  OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package me.noud02.akatsuki.commands
+
+import khttp.extensions.fileLike
+import me.noud02.akatsuki.annotations.Load
+import me.noud02.akatsuki.entities.Command
+import me.noud02.akatsuki.entities.Context
+import org.apache.commons.io.IOUtils
+import java.io.File
+import java.io.FileOutputStream
+
+@Load
+class Woow : Command() {
+    override fun run(ctx: Context) {
+        val temp = File.createTempFile("image", "png")
+        temp.deleteOnExit()
+        val out = FileOutputStream(temp)
+        IOUtils.copy(ctx.msg.attachments.getOrNull(0)?.inputStream ?: ctx.getLastImage() ?: return ctx.send("No images found!"), out)
+
+
+        val req = khttp.post(
+                "${
+                if (ctx.client.config.backend.ssl) "https" else "http"
+                }://${
+                ctx.client.config.backend.host
+                }${
+                if (ctx.client.config.backend.port != 80) ":${ctx.client.config.backend.port}" else ""
+                }/api/woow",
+                files = listOf(temp.fileLike())
+        )
+
+        ctx.channel.sendFile(req.content, "woow.png", null).queue()
+    }
+}
