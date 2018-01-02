9aad381e79b5aaf63746b41e8cf67740da635cf9
noud02 <noud.kerver@hotmail.nl>
2018-01-02 19:55:45 +0100

    add yandere command

---
+++ src/main/kotlin/me/noud02/akatsuki/commands/Yandere.kt
@@ -0,0 +1,60 @@
+/*
+ *  Copyright (c) 2017 Noud Kerver
+ *
+ *  Permission is hereby granted, free of charge, to any person
+ *  obtaining a copy of this software and associated documentation
+ *  files (the "Software"), to deal in the Software without
+ *  restriction, including without limitation the rights to use,
+ *  copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following
+ *  conditions:
+ *
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *  OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package me.noud02.akatsuki.commands
+
+import me.noud02.akatsuki.annotations.Argument
+import me.noud02.akatsuki.annotations.Load
+import me.noud02.akatsuki.entities.Command
+import me.noud02.akatsuki.entities.Context
+import me.noud02.akatsuki.utils.I18n
+import net.dv8tion.jda.core.EmbedBuilder
+
+@Load
+@Argument("query", "string")
+class Yandere : Command() {
+    override val nsfw = true
+    override val desc = "Search for (lewd) images on yande.re"
+
+    override fun run(ctx: Context) {
+        val query = ctx.args["query"] as String
+
+        if (query.indexOf("loli") > -1)
+            return ctx.send(I18n.parse(ctx.lang.getString("loli_is_illegal"), mapOf("username" to ctx.author.name)))
+
+        val req = khttp.get("https://yande.re/post.json", params = mapOf("limit" to "100", "tags" to query))
+
+        val json = req.jsonArray.getJSONObject(Math.floor(Math.random() * req.jsonArray.length()).toInt())
+
+        if (json.getString("tags").indexOf("loli") > -1)
+            return ctx.send(I18n.parse(ctx.lang.getString("loli_is_illegal"), mapOf("username" to ctx.author.name)))
+
+        val embed = EmbedBuilder().apply {
+            setImage(json.getString("file_url"))
+        }
+
+        ctx.send(embed.build())
+    }
+}
