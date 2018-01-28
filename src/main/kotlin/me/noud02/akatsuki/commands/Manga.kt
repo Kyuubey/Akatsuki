6e15b4d86354af602ef9bc1af59ca9870522b7bb
noud02 <noud.kerver@hotmail.nl>
2018-01-28 15:32:29 +0100

    add manga command

--- src/main/kotlin/me/noud02/akatsuki/commands/Manga.kt
+++ src/main/kotlin/me/noud02/akatsuki/commands/Manga.kt
@@ -1,3 +1,28 @@
+/*
+ *   Copyright (c) 2017 Noud Kerver
+ *
+ *   Permission is hereby granted, free of charge, to any person
+ *   obtaining a copy of this software and associated documentation
+ *   files (the "Software"), to deal in the Software without
+ *   restriction, including without limitation the rights to use,
+ *   copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the
+ *   Software is furnished to do so, subject to the following
+ *   conditions:
+ *
+ *   The above copyright notice and this permission notice shall be
+ *   included in all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *   OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 @file:Suppress("DEPRECATION")
 
 package me.noud02.akatsuki.commands
@@ -8,15 +33,17 @@ import me.noud02.akatsuki.annotations.Load
 import me.noud02.akatsuki.entities.Command
 import me.noud02.akatsuki.entities.Context
 import net.dv8tion.jda.core.EmbedBuilder
-import okhttp3.*
+import okhttp3.Credentials
+import okhttp3.HttpUrl
+import okhttp3.Request
 import org.apache.commons.lang3.StringEscapeUtils
 import org.json.XML
 import java.awt.Color
 
 @Load
-@Argument("anime", "string")
-class Anime : Command() {
-    override val desc = "Search for anime on MyAnimeList"
+@Argument("manga", "string")
+class Manga : Command() {
+    override val desc = "Search for manga on MyAnimeList"
 
     override fun run(ctx: Context) {
         val res = Akatsuki.instance.okhttp.newCall(Request.Builder().apply {
@@ -24,9 +51,9 @@ class Anime : Command() {
                 scheme("https")
                 host("myanimelist.net")
                 addPathSegment("api")
-                addPathSegment("anime")
+                addPathSegment("manga")
                 addPathSegment("search.xml")
-                addQueryParameter("q", ctx.args["anime"] as String)
+                addQueryParameter("q", ctx.args["manga"] as String)
             }.build())
 
             val mal = Akatsuki.instance.config.api.myanimelist
@@ -38,18 +65,22 @@ class Anime : Command() {
 
         val json = XML
                 .toJSONObject(res.body()!!.string())
-                .getJSONObject("anime")
+                .getJSONObject("manga")
 
         val entry = json.getJSONObject("entry") ?: json.getJSONArray("entry").getJSONObject(0)
 
         val embed = EmbedBuilder().apply {
             setTitle(entry.getString("title"))
 
-            descriptionBuilder.append("${entry.getDouble("score")} \u2606 | ${entry.getInt("episodes")} ${
+            descriptionBuilder.append("${
+            entry.getDouble("score")
+            } \u2606 | ${
+            entry.getInt("volumes")
+            } \uD83D\uDCDA ${
+            entry.getInt("chapters")
+            } ${
             when (entry.getString("type").toLowerCase()) {
-                "tv" -> "\uD83D\uDCFA"
-
-                "movie" -> "\uD83C\uDF7F"
+                "manga" -> "\uD83D\uDCD6"
                 else -> "?"
             }
             } ${entry.getString("status")} | ${
