72bed213598d0af117ca9903fcf9b58f41cfc323
noud02 <noud.kerver@hotmail.nl>
2018-01-30 16:19:04 +0100

    make command classes abstract

--- src/main/kotlin/me/noud02/akatsuki/entities/Command.kt
+++ src/main/kotlin/me/noud02/akatsuki/entities/Command.kt
@@ -25,7 +25,7 @@
 
 package me.noud02.akatsuki.entities
 
-open class Command {
+abstract class Command {
     val subcommands = mutableMapOf<String, Command>()
     open val name = ""
     open val desc = ""
@@ -34,7 +34,7 @@ open class Command {
     open val guildOnly = false
     open val nsfw = false
 
-    open fun run(ctx: Context) = ctx.send("Empty command")
+    abstract fun run(ctx: Context)
 
     fun addSubcommand(cmd: Command, name: String? = null) {
         subcommands[(name ?: if (cmd.name.isEmpty()) cmd::class.simpleName ?: return else cmd.name).toLowerCase()] = cmd
