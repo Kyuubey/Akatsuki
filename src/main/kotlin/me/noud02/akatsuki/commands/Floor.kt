1ee18033c2af743f5bba418480a314dd5050be3a
yuwui <me@yui.pet>
2018-02-13 15:43:05 +0100

    add floor is lava memegen

---
+++ src/main/kotlin/me/noud02/akatsuki/commands/Floor.kt
@@ -0,0 +1,64 @@
+/*
+ *   Copyright (c) 2017-2018 Noud Kerver
+ *
+ *   Permission is hereby granted, free of charge, to any person
+ *   obtaining a copy of this software and associated documentation
+ *   files (the "Software"), to deal in the Software without
+ *   restriction, including without limitation the rights to use,
+ *   copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the
+ *   Software is furnished to do so, subject to the following
+ *   conditions:
+ *
+ *   The above copyright notice and this permission notice shall be
+ *   included in all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *   OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package me.noud02.akatsuki.commands
+
+import me.noud02.akatsuki.Akatsuki
+import me.noud02.akatsuki.annotations.Argument
+import me.noud02.akatsuki.annotations.Arguments
+import me.noud02.akatsuki.annotations.Load
+import me.noud02.akatsuki.entities.Command
+import me.noud02.akatsuki.entities.Context
+import net.dv8tion.jda.core.entities.Member
+import okhttp3.HttpUrl
+import okhttp3.Request
+
+@Load
+@Arguments(
+        Argument("user", "user"),
+        Argument("text", "string")
+)
+class Floor : Command() {
+    override val desc = "The floor is lava meme generator."
+
+    override fun run(ctx: Context) {
+        val member = ctx.args["user"] as Member
+        val res = Akatsuki.instance.okhttp.newCall(Request.Builder().apply {
+            url(HttpUrl.Builder().apply {
+                scheme(if (Akatsuki.instance.config.backend.ssl) "https" else "http")
+                host(Akatsuki.instance.config.backend.host)
+                port(Akatsuki.instance.config.backend.port)
+                addPathSegment("api")
+                addPathSegment("floor")
+                addQueryParameter("text", "the floor is ${ctx.args["text"]}")
+                addQueryParameter("image", member.user.avatarUrl)
+            }.build())
+        }.build()).execute()
+
+        ctx.event.channel.sendFile(res.body()!!.byteStream(), "floorislava.png", null).queue {
+            res.close()
+        }
+    }
+}
