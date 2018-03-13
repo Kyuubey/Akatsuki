a5205d51db9061f825a767b3a2629a1b4afcd794
yuwui <me@yui.pet>
2018-03-13 20:30:24 +0100

    add back nekomimi command

---
+++ src/main/kotlin/me/noud02/akatsuki/commands/Nekomimi.kt
@@ -0,0 +1,51 @@
+/*
+ *   Copyright (c) 2017-2018 Noud Kerver
+ *
+ *   Permission is hereby granted, free of charge, to any person
+ *   obtaining a copy of this software and associated documentation
+ *   files (the "Software"), to deal in the Software without
+ *   restriction, including without limitation the rights to use,
+ *   copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the
+ *   Software is furnished to do so, subject to the following
+ *   conditions:
+ *
+ *   The above copyright notice and this permission notice shall be
+ *   included in all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *   OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package me.noud02.akatsuki.commands
+
+import io.sentry.Sentry
+import me.noud02.akatsuki.annotations.Load
+import me.noud02.akatsuki.entities.Command
+import me.noud02.akatsuki.entities.Context
+import me.noud02.akatsuki.utils.Http
+import org.json.JSONObject
+
+@Load
+class Nekomimi : Command() {
+    override val desc = "Get a random catgirl"
+
+    override fun run(ctx: Context) {
+        Http.get("https://nekos.life/api/v2/img/neko").thenAccept { res ->
+            val json = JSONObject(res.body()!!.string())
+
+            ctx.send(json.getString("url"))
+            res.close()
+        }.thenApply {}.exceptionally {
+            ctx.logger.error("Error while trying to get a random catgirl from nekos.life", it)
+            ctx.sendError(it)
+            Sentry.capture(it)
+        }
+    }
+}
